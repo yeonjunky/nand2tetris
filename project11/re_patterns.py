94f308183084c3f610ef2cdb81b9c1a2ff73cc10
yeonjunky <yeonjunky@gmail.com>
2024-05-11 18:50:40 +0900

    solved double issue

--- project11/re_patterns.py
+++ project11/re_patterns.py
@@ -3,12 +3,12 @@ import re
 comment_pattern = re.compile(r'//[^\n]*\n|/\*(.*?)\*/', re.DOTALL|re.MULTILINE)
 empty_text = re.compile(r"\s*")
 
-
-keyword = re.compile(r"\s*(class|method|function|constructor|int|boolean|char|void|var|static|field|let|do|if|else|while|return|true|false|null|this)\s*")
+# do miscompile double to 'do' and 'uble'. solved adding $ 
+keyword = re.compile(r"^\s*(class|method|function|constructor|int|boolean|char|void|var|static|field|let|do$|if|else|while|return|true|false|null|this)\s*")
 
 symbol_pattern = re.compile(r"\s*([{}()\[\].,;+\-*/&|<>=~])\s*")
 
-identifier_pattern = re.compile(r"\s*([a-zA-z_][a-zA-Z0-9]*)\s*")
+identifier_pattern = re.compile(r"\s*([a-zA-Z_][a-zA-Z0-9_]*)\s*")
 
 int_const_pattern = re.compile(r"\s*(\d+)\s*")
 
