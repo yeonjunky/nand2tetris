8d5d594065a16afbf00890bb4162035e97d4b5ad
yeonjunky <yeonjunky@gmail.com>
2024-05-14 20:38:21 +0900

    script to compile all test files

---
+++ project11/compile-all.sh
@@ -0,0 +1,18 @@
+echo "compiling Seven"
+python3 JackCompiler.py Seven/
+
+echo "compiling ConvertToBin"
+python3 JackCompiler.py ConvertToBin/
+
+echo "compiling Square"
+python3 JackCompiler.py Square/
+
+echo "compiling Average"
+python3 JackCompiler.py Average/
+
+echo "compiling Pong"
+python3 JackCompiler.py Pong/
+
+echo "compiling ComplexArrays"
+python3 JackCompiler.py ComplexArrays/
+ 
